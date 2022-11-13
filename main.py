de42b135f52039da9211ea78f7e165847ca42af4
Cheikh Bassirou Syll <bassco70@gmail.com>
2022-11-13 13:00:43 +0100

    generer une liste qui a autant de underscore que de lettres dans le mot selectionne par hasard.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import random
 word_list = ['putain', 'flacide', 'fils']
 chosen_word = random.choice(word_list)
+print(f'le mot est {chosen_word}')
+""" creer une liste vide display. Pour chaque lettre dans la liste chosen_word, ajouter un "_" dasn display. Si chosen_word est 'apple', display devrait etre ['_', '_', '_', '_', '_'] representant chaque lettre a deviner
+"""
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += '_'
+print(display)
 
 guess = input("Devine une lettre : ").lower()
 
-for letter in chosen_word:
+""" on boucle sur chaque position dans chosen_word; Si chaqque lettre a cette position correspond a 'guess' donc met cette lettre dans isplay sur cette position. Si l'utilisateur devine 'p' et le mot choisi etait 'apple', donc display devrait etre ['_', 'p', 'p', '_', '_'] """
+
+for position in range(word_length):
+    letter = chosen_word[position]
     if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+""" Affiche display et vous devez voir la lettre devinee correctement dans sa position et les autres remplaces par '_'"""
+print(display)
